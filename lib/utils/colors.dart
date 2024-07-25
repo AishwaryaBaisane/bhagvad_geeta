66c9b1701e4c1650b89cfe0afdb54bd5a7433383
Admin <baisanehira@gmail.com>
2024-07-25 21:30:35 +0530

    7/25/2024

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,7 +1,12 @@
 
 import 'dart:ui';
 
+// Light Theme
+
 Color Orange = Color(0xffeb6c03);
 Color LightOrange = Color(0xffde9024);
+
+// Dark Theme
+
 Color DarkBlack = Color(0xff0c0c0c);
 Color LightBlack = Color(0xff1f1f1f);
